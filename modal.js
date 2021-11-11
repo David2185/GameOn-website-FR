891b9819895488dc02eda587d338ca3174c5c6ee
David <david.alvesdebrito@gmail.com>
2021-11-11 20:14:30 +0100

    test function closeModal

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.getElementById("close-modale");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//function close Modale
+
+function closeModalBtn() {
+  closeModal.style.display = "none";
+}
+
 //début du p4
+
+//Fermer la modale
+
+closeModal.addEventListener("click", closeModalBtn);
